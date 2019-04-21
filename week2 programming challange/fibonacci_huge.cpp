82038c643729bf24321b6d8ad4ea118dadc2c297
Vijendra Kumar <43384082+vijevira@users.noreply.github.com>
2019-04-21 23:00:09 +0530

    Create fibonacci_huge.cpp

---
+++ week2 programming challange/fibonacci_huge.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+long long get_pisano_period_length(long long m) {
+    long long F1 = 0, F2 = 1, F = F1 + F2;
+    for (int i = 0; i < m * m; i++) {
+        F = (F1 + F2) % m;
+        F1 = F2;
+        F2 = F;
+        if (F1 == 0 && F2 == 1) return i + 1;
+    }
+}
+long long get_fibonacci_huge_naive(long long n, long long m) {
+    long long remainder = n % get_pisano_period_length(m);
+
+    long long F1 = 0, F2 = 1, F = remainder;
+    for (int i = 1; i < remainder; i++) {
+        F = (F1 + F2) % m;
+        F1 = F2;
+        F2 = F;
+    }
+    return F % m;
+}
+
+int main() {
+    long long n, m;
+    std::cin >> n >> m;
+    std::cout << get_fibonacci_huge_naive(n, m) << '\n';
+}
